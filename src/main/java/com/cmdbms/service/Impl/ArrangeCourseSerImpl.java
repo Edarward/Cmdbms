fcce10ed193220a328711ee1123a4b83728c93dd
NeiZK <41323680+NeiZK@users.noreply.github.com>
2019-05-13 10:01:51 +0800

    1

--- src/main/java/com/cmdbms/service/Impl/ArrangeCourseSerImpl.java
+++ src/main/java/com/cmdbms/service/Impl/ArrangeCourseSerImpl.java
@@ -1,31 +1,37 @@
 package com.cmdbms.service.Impl;
 
+import com.cmdbms.mapper.ArgcoureMapper;
 import com.cmdbms.model.Argcoure;
 import com.cmdbms.service.ArrangeCourseSer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class ArrangeCourseSerImpl {
+public class ArrangeCourseSerImpl implements ArrangeCourseSer{
 
-//    int insertOne(Argcoure course){
-//
-//
-//    }
-//
-//    List selectOne(){
-//
-//
-//    }
-//
-//    int updateOne(Argcoure course){
-//
-//    }
-//
-//    int deleteOne(int id){
-//
-//
-//    }
+    @Autowired
+    ArgcoureMapper argcoureMapper;
+
+    @Override
+    public int insertOne(Argcoure course){
+        return  argcoureMapper.insert(course);
+    }
+
+    @Override
+    public List selectOne(){
+        return argcoureMapper.selectAll();
+    }
+
+    @Override
+    public int updateOne(Argcoure course){
+        return argcoureMapper.updateByPrimaryKey(course);
+    }
+
+    @Override
+    public int deleteOne(int id){
+        return argcoureMapper.deleteByPrimaryKey(id);
+    }
 
 }
