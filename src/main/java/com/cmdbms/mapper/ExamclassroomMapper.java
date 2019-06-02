076c9773d6759f314c32300aa611e7b76bffc8ac
dongbobhvv <48619890+dongbobhvv@users.noreply.github.com>
2019-06-02 21:58:18 +0800

    1

--- src/main/java/com/cmdbms/mapper/ExamclassroomMapper.java
+++ src/main/java/com/cmdbms/mapper/ExamclassroomMapper.java
@@ -1,8 +1,10 @@
 package com.cmdbms.mapper;
 
 import com.cmdbms.model.Examclassroom;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface ExamclassroomMapper {
 
     int deleteByPrimaryKey(Integer id);
--- src/main/java/com/cmdbms/mapper/ExamclassroomuseMapper.java
+++ src/main/java/com/cmdbms/mapper/ExamclassroomuseMapper.java
@@ -1,8 +1,10 @@
 package com.cmdbms.mapper;
 
 import com.cmdbms.model.Examclassroomuse;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface ExamclassroomuseMapper {
 
     int deleteByPrimaryKey(Integer id);
